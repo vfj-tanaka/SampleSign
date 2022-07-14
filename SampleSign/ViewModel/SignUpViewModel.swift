6947484c1555041f2494c47c848283c7aca37702
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-07-14 16:37:07 +0900

    Firebaseをセットアップし、SignUpVCからViewModelにインプットする処理をしました。

--- SampleSign/ViewModel/SignUpViewModel.swift
+++ SampleSign/ViewModel/SignUpViewModel.swift
@@ -6,7 +6,49 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-final class SignUpViewModel {
+protocol signUpInput {
+    var mailTextObserver: AnyObserver<String> { get }
+    var passTextObserver: AnyObserver<String> { get }
+    var confirmTextObserver: AnyObserver<String> { get }
+    var confirmValidation: AnyObserver<Bool> { get }
+}
+
+protocol signUpOutput {
     
+}
+
+final class SignUpViewModel: signUpInput, signUpOutput {
+    
+    private let disposeBag = DisposeBag()
+    
+    private let _mailText = PublishRelay<String>()
+    lazy var mailTextObserver: AnyObserver<String> = .init { event in
+        guard let e = event.element else { return }
+        self._mailText.accept(e)
+    }
+    
+    private let _passText = PublishRelay<String>()
+    lazy var passTextObserver: AnyObserver<String> = .init { event in
+        guard let e = event.element else { return }
+        self._passText.accept(e)
+    }
+    
+    private let _confirmText = PublishRelay<String>()
+    lazy var confirmTextObserver: AnyObserver<String> = .init { event in
+        guard let e = event.element else { return }
+        self._confirmText.accept(e)
+    }
+    
+    private let _confirmValidation = PublishRelay<Bool>()
+    lazy var confirmValidation: AnyObserver<Bool> = .init { event in
+        guard let e = event.element else { return }
+        self._confirmValidation.accept(e)
+    }
+    
+    init() {
+        
+    }
 }
