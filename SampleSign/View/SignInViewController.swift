d0d490cdcbb032eba3ea64904a9f9adb6959adbe
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-07-14 17:15:17 +0900

    SignInVCをStoryboardと繋いでViewModelへのインプット処理を作成しました。

--- SampleSign/View/SignInViewController.swift
+++ SampleSign/View/SignInViewController.swift
@@ -6,10 +6,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxOptional
 
 final class SignInViewController: UIViewController {
     
+    @IBOutlet private weak var mailTextField: UITextField!
+    @IBOutlet private weak var passTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
+    
+    private let viewModel = SignInViewModel()
+    private lazy var input: signInInput = viewModel
+    private lazy var output: signInOutput = viewModel
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bindInputStream()
+        bindOutputStream()
+    }
+    
+    private func bindInputStream() {
+        
+        let mailTextObsavable = mailTextField.rx.text
+            .distinctUntilChanged().filterNil().filter { $0.isNotEmpty }
+        let passTextObsavable = passTextField.rx.text
+            .distinctUntilChanged().filterNil().filter { $0.isNotEmpty }
+        
+        mailTextObsavable.bind(to: input.mailTextObserver).disposed(by: disposeBag)
+        passTextObsavable.bind(to: input.passTextObserver).disposed(by: disposeBag)
+    }
+    
+    private func bindOutputStream() {
+        
+    }
+    
+    @IBAction private func login(_ sender: Any) {
+    }
+    
+    @IBAction private func gotoSignUp(_ sender: Any) {
     }
 }
