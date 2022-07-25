7ed5b985162327f0acf93eca9b2b1205a8bf2aef
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-07-25 15:53:39 +0900

    userNameを追加しFirebaseの処理も記載しました。

--- SampleSign/ViewModel/SignUpViewModel.swift
+++ SampleSign/ViewModel/SignUpViewModel.swift
@@ -10,6 +10,7 @@ import RxSwift
 import RxCocoa
 
 protocol signUpInput {
+    var userNameTextObserver: AnyObserver<String> { get }
     var mailTextObserver: AnyObserver<String> { get }
     var passTextObserver: AnyObserver<String> { get }
     var confirmTextObserver: AnyObserver<String> { get }
@@ -23,6 +24,12 @@ protocol signUpOutput {
 final class SignUpViewModel: signUpInput, signUpOutput {
     
     private let disposeBag = DisposeBag()
+    
+    private let _userNameText = PublishRelay<String>()
+    lazy var userNameTextObserver: AnyObserver<String> = .init { event in
+        guard let e = event.element else { return }
+        self._userNameText.accept(e)
+    }
     
     private let _mailText = PublishRelay<String>()
     lazy var mailTextObserver: AnyObserver<String> = .init { event in
